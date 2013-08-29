336018e79880366bcfbca1c6595c8893df8c2490
Bulat Abdullin <bulatabdullin@gmail.com>
2013-08-29 11:02:55 +0400

    added transform function

--- src/jquery.splittedinput.js
+++ src/jquery.splittedinput.js
@@ -8,7 +8,9 @@ $.fn.extend({
         _this.settings = $.extend({
             template: "{10}",
             class: "",
-            autoJump: true
+            autoJump: true,
+            pattern: "",
+            transform: function(string){return string;}
         }, settings);
 
         _this.regInput = function(input){
@@ -16,9 +18,13 @@ $.fn.extend({
         };
 
         _this.replacedString = settings.template.replace(/\{(\d*)\}/g, function(matched, size){
-            var el = $('<input>');
+            var el = $('<input>').attr('type', 'text');
+
             if (size)
                 el.attr('maxlength', size)
+
+            if (settings.pattern) 
+                el.attr('pattern', settings.pattern)
 
             var helperDiv = $('<div>');
             var toReturn = helperDiv.append(el).html()
@@ -29,15 +35,30 @@ $.fn.extend({
         $(this).replaceWith(_this.replacedString);
 
         _this.inputs = []
+        _this.buffer = []
         _this.parent.find('input').each(function(index, item){
             _this.inputs.push(item);
             if (_this.settings.autoJump)
                 $(item).keypress(function(e){
-                    if ($(this).val().length >= ($(this).attr('maxlength') - 1)) {
+                    var k = e.which;
+                    _this.buffer.push(settings.transform(String.fromCharCode(k)));
+                    $(this).val(_this.buffer.join(''))
+                    if ($(this).val().length >= ($(this).attr('maxlength'))) {
                         if (_this.inputs[index + 1])
-                            _this.inputs[index + 1].focus()
+                        {
+                            _this.inputs[index + 1].focus();
+                            _this.buffer = [];
+                        }
+                        else {
+                            _this.buffer = [];
+                            $(this).blur();
+                        }
                     }
-                })
+                    e.preventDefault();
+                });
+                $(item).focus(function(e){
+                    $(this).val('');
+                });
         });
     }
 });
